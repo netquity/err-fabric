b2c9dfc3a5fecaeb5e42292479019ae77e8e0677
Alex Sheluchin <alex@netquity.com>
2016-09-17 17:15:44 -0400

    Create initial err-fabric plugin

---
+++ README.md
@@ -0,0 +1,68 @@
+# err-fabric
+
+This plugin allows you to invoke [Fabric](http://www.fabfile.org/) commands using [Errbot](http://errbot.io/en/latest/).
+
+From the Fabric documentation:
+
+> Fabric is:
+>
+> * A tool that lets you execute arbitrary Python functions via the command line;
+> * A library of subroutines (built on top of a lower-level library) to make executing shell commands over SSH easy and Pythonic.
+
+## Installation
+
+You must provide your own fabfile. For an overview on how to use Fabric, see their [Tutorial](http://docs.fabfile.org/en/latest/tutorial.html).
+
+You must also provide your own SSH config file. See [Leveraging native SSH config
+files](http://docs.fabfile.org/en/latest/usage/execution.html#leveraging-native-ssh-config-files) for details. Don't forget to make sure that your bot server is authorized access to each of the hosts you define. The `HOSTNAMES` variable shown below should reference hosts defined in your SSH config.
+
+Make sure Python 2 is installed somewhere on your system, as Fabric is not yet compatible with Python 3.
+
+### Example configuration
+
+In your environment, define all required variables:
+
+```
+# Path to the Python 2 binary
+export PYTHON2_PATH="${APP_DIR}/env2/bin/python2.7"
+# Path to the Fabric binary
+export FABRIC_PATH="${APP_DIR}/env2/bin/fab"
+# Path to where your fabfile is
+export FABFILE_PATH='/home/web/fabfile/'
+
+# A list of the commands from your fabfile that should be executable through your Errbot
+export ALLOWED_TASKS='
+    bootstrap
+    deploy
+    status
+'
+# A list of the hosts from your fabfile that you should be able to execute commands against
+export HOSTNAMES='
+    server_a
+    server_b
+'
+```
+
+## Use
+
+* Check the status of `server_a`:
+
+```
+!fab -H server_a status
+```
+
+* Deploy the result of pulling branch `x` into the `develop` branch:
+
+```
+!fab -H server_a branch:develop pull:x deploy
+```
+
+Note that the above executes several commands (`branch`, `pull`, `deploy`) defined in the fabfile.
+
+### Caveats
+
+* Any commands that produce a prompt for addition input are not supported.
+* You can only execute commands against a single host at this time.
+* This plugin was built specifically for use with the Slack backend. Other backends may work, but are not guarenteed.
+
+Inspired by [hubot-fabric](https://github.com/tracelytics/hubot-fabric).
---
+++ fabric.py
@@ -0,0 +1,186 @@
+import argparse
+import logging
+import os
+import subprocess
+import sys
+
+from errbot import BotPlugin, botcmd, arg_botcmd, utils
+
+FABFILE_PATH = os.getenv('FABFILE_PATH')
+FABRIC_PATH = os.getenv('FABRIC_PATH')
+PYTHON2_PATH = os.getenv('PYTHON2_PATH')
+
+ALLOWED_TASKS = os.getenv('ALLOWED_TASKS').split()
+HOSTNAMES = os.getenv('HOSTNAMES').split()
+
+MAX_LENGTH_MESSAGE = 4000 - 100 - 6  # We use 6 graves for preformatted wrapping; 100 for a bit of buffer room
+MAX_LENGTH_CARD = 8000  # Preformatted wrapping doesn't work on cards yet
+
+logger = logging.getLogger(__file__)
+
+
+def chunks(l, n):
+    """Return a list containing successive n-sized chunks from l."""
+    n = max(1, n)
+    return [l[i:i + n] for i in range(0, len(l), n)]
+
+
+class Fabric(BotPlugin):
+    """Execute commands in the fabfile"""
+    @arg_botcmd('-H', type=str, dest='host', choices=HOSTNAMES, required=True)
+    @arg_botcmd(
+        'tasks',
+        nargs=argparse.REMAINDER,
+        choices=ALLOWED_TASKS,
+        help='The tasks to be executed against the selected host.',
+    )
+    def fab(self, message, host, tasks):
+        try:
+            Fabric.validate_whole_input(message.body)
+        except utils.ValidationException as exc:
+            failure_message = "Invalid input command; check that it doesn't contain any shell meta-characters"
+            logger.exception(failure_message)
+            return self.send_card(
+                in_reply_to=message,
+                body=failure_message,
+                color='red',
+            )
+
+        for task in Fabric.extract_task_names(tasks):
+            try:
+                Fabric.validate_task(task)
+            except utils.ValidationException as exc:
+                logger.exception('Invalid task: %s' % task)
+                return self.send_card(
+                    in_reply_to=message,
+                    fields=(('Invalid Task', task),),
+                    color='red',
+                )
+
+        yield 'Your message is now processing...'
+
+        exc_message = None
+        exc_tuple = None
+        try:
+            completed_process = Fabric.execute_task(
+                host,
+                tasks,
+            )
+        except OSError as exc:
+            exc_message = 'Bad file path: either the Python2 or Fabric binary, or fabfile itself'
+            exc_tuple = sys.exc_info()
+        except ValueError as exc:
+            exc_message = 'A bad argument was provided to Popen'
+            exc_tuple = sys.exc_info()
+        except subprocess.TimeoutExpired as exc:
+            exc_message = 'The task was unable to finish before the set timeout expired'
+            exc_tuple = sys.exc_info()
+        except subprocess.CalledProcessError as exc:
+            # FIXME: this is problematic because it doesn't state which task, and some of them may have succeeded
+            exc_message = 'The task exited with a non-zero exit code; unable to complete'
+            exc_tuple = sys.exc_info()
+        except subprocess.SubprocessError as exc:
+            exc_message = 'An ambiguous error occurred while calling Subprocess'
+            exc_tuple = sys.exc_info()
+        else:
+            try:
+                if len(completed_process.stdout) <= MAX_LENGTH_MESSAGE:
+                    yield '```%s```' % completed_process.stdout
+                    return
+                elif len(completed_process.stdout) <= MAX_LENGTH_CARD:
+                    return self.send_card(
+                        in_reply_to=message,
+                        body=completed_process.stdout,
+                        color='green',
+                    )
+                else:  # Neither of the normal ways of sending messages works; we have to chunk the message
+                    message_chunks = chunks(completed_process.stdout, MAX_LENGTH_MESSAGE)
+                    yield 'Response broken down into %s chunks...' % len(message_chunks)
+
+                    for i, chunk in enumerate(message_chunks):
+                        yield 'Chunk %s/%s' % (i + 1, len(message_chunks))
+                        # We only have to do this because Errbot won't apply the graves if it does the splitting itself
+                        yield '```%s```' % chunk
+
+                    return
+            except ValueError as exc:
+                exc_message = "Missing or invalid arguments provided to Errbot's send_card()"
+                exc_tuple = sys.exc_info()
+        finally:
+            if exc_message is not None:  # The task didn't work
+                exception = exc_tuple[1]
+                logger.exception(
+                    exc_message,
+                    exc_info=exc_tuple,
+                )
+                self.send_card(
+                    in_reply_to=message,
+                    body=exc_message,
+                    color='red',
+                )
+
+                if hasattr(exception, 'stdout'):
+                    if len(exception.stdout) <= MAX_LENGTH_MESSAGE:
+                        yield '```%s```' % exception.stdout
+                    else:
+                        logger.error(
+                            exception.stdout,
+                        )
+                        self.send_card(
+                            in_reply_to=message,
+                            body='The error log is too long to display here; check on the server',
+                            color='red',
+                        )
+
+    @staticmethod
+    def validate_task(task_name):
+        """Confirm that the given task is allowed"""
+        if task_name not in ALLOWED_TASKS:
+            raise utils.ValidationException(
+                'Task %s is not one of the ALLOWED_TASKS. No tasks have been executed.' % task_name,
+            )
+
+    @staticmethod
+    def extract_task_names(tasks):
+        """Receive a list of tasks, possibly containing arguments, and return only task names"""
+        task_names = []
+        for task in tasks:
+            task_names.append(task.split(':')[0])
+
+        return task_names
+
+    @staticmethod
+    def validate_whole_input(input_string):
+        """Make sure that no shell meta-characters are anywhere in the input"""
+        META_CHARS = list(';|`$()&<>') + ['--']  # Include `--` to prevent Arbitrary remote shell commands
+
+        for meta_char in META_CHARS:
+            if meta_char in input_string:
+                raise utils.ValidationException(
+                    "Illegal character=%s. "
+                    "Shell meta-characters and Fabric's arbitrary remote shell command '--' not allowed."
+                    % meta_char
+                )
+
+    @staticmethod
+    def execute_task(host, tasks):
+        """Call Fabric to execute tasks against a host
+
+        Return: CompletedProcess instance
+        """
+        # TODO: add support for groups, multiple hosts
+        # TODO: add support for providing input data from team files
+        return subprocess.run(
+            [
+                PYTHON2_PATH,
+                FABRIC_PATH,
+                '--abort-on-prompts',
+                '--hosts=%s' % host,
+                '--fabfile=%s' % FABFILE_PATH,
+                *tasks,
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,  # Combine out/err into stdout; stderr will be None
+            universal_newlines=True,
+            check=True,
+        )
